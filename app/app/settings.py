b995104912e15a00a4b8dbc221507617d53376eb
stupns <stupns@i.ua>
2024-01-02 20:49:15 -0500

    finished task building the backend

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+
+    'rest_framework',
+
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
