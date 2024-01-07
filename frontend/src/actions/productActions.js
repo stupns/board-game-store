a4c10a3820e2fa903d3a3c85098be64a79fc48d9
stupns <stupns@i.ua>
2024-01-07 16:53:07 -0500

    successfull update code for a bringing intro home screen.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,7 @@ import {PRODUCT_LIST_REQUEST,
         PRODUCT_LIST_FAIL
 } from '../constants/productConstants';
 
-const listProducts = () => async (dispatch) =>{
+export const listProducts = () => async (dispatch) =>{
 try {
     dispatch({type : PRODUCT_LIST_REQUEST})
     const {data} = await axios.get(`http://localhost:8000/api/products/`)
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,40 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
+import {useDispatch, useSelector } from 'react-redux';
+import {listProducts} from '../actions/productActions';
 
-import axios from 'axios';
 
 
 
 function HomeScreen() {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList;
 
     useEffect(()=>{
-        async function fetchProducts() {
-            try {
-                const {data} = await axios.get(`http://localhost:8000/api/products/`)
-                setProducts(data)
-            } catch (error) {
-                console.error('Error fetching products:', error);
-            }
+        dispatch(listProducts())
 
-        }
-
-        fetchProducts()
-
-    }, [])
+    }, [dispatch])
 
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
+            {loading ? <h2>Loading...</h2>
+                : error ? <h3>{error}</h3>
+                    :
+                    <Row>
+                        {products.map(product => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
 
-                ))}
-            </Row>
+                        ))}
+                    </Row>
+
+            }
         </div>
     );
 }
