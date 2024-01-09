63ac3ff1564d9a3da55f8ced7450552d1e60394a
stupns <stupns@i.ua>
2024-01-08 23:17:17 -0500

    finish. need create nginx.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useParams, useLocation} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {addToCart} from '../actions/cartActions';
+// import Message from '../components/Message';
+// import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap';
+// import {Link} from 'react-bootstrap';
 
 function CartScreen() {
+
+    const location = useLocation()
+    const {id} = useParams()
+    const productId = id
+    const qty = parseInt(new URLSearchParams(location.search).get('qty'))
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+    const dispatch = useDispatch()
+
+    console.log('cartItems', cartItems)
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
     return (
         <div>
             Cart
