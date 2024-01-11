2ee77d67519099779aa01f0eebdd0bb802136de6
stupns <stupns@i.ua>
2024-01-10 20:56:59 -0500

    completed user serial.

--- app/base/views.py
+++ app/base/views.py
@@ -1,5 +1,5 @@
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializersWithToken
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -11,8 +11,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+
+        serializer = UserSerializersWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
 
@@ -24,6 +26,13 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(['GET'])
 def getRoutes(request):
     return Response('Hello')
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
