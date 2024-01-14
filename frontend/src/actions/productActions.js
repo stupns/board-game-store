d49c51b322e1cecb47f782458b55f24d120a40d4
stupns <stupns@i.ua>
2024-01-13 20:25:43 -0500

    completed user login screen.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -11,7 +11,7 @@ import {PRODUCT_LIST_REQUEST,
 export const listProducts = () => async (dispatch) =>{
 try {
     dispatch({type : PRODUCT_LIST_REQUEST})
-    const {data} = await axios.get(`http://localhost:8000/api/products/`)
+    const {data} = await axios.get(`/api/products/`)
 
     dispatch({
         type: PRODUCT_LIST_SUCCESS,
@@ -31,7 +31,7 @@ try {
 export const listProductDetails = (id) => async (dispatch) =>{
 try {
     dispatch({type : PRODUCT_DETAILS_REQUEST})
-    const {data} = await axios.get(`http://localhost:8000/api/products/${id}`)
+    const {data} = await axios.get(`/api/products/${id}`)
 
     dispatch({
         type: PRODUCT_DETAILS_SUCCESS,
