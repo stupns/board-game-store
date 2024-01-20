745a03330107909bfcc06dc28d86ec93dcbd42b7
stupns <stupns@i.ua>
2024-01-20 15:36:33 -0500

    Add funct. get user by id and update user endpoints.

--- app/base/urls/user_urls.py
+++ app/base/urls/user_urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('profile/update/', views.updateUserProfile, name="users-profile-update"),
     path('', views.getUsers, name="users"),
 
+    path('<str:pk>/', views.getUsersById, name="user"),
+    path('update/<str:pk>/', views.updateUser, name="user-update"),
     path('delete/<str:pk>/', views.deleteUser, name="user-delete"),
 
 ]
